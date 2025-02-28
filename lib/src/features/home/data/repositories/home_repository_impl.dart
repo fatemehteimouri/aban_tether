64488affb974e9e29590f50eac7bcad9385acd89
sepehr13494 <marash.sepehr13494@gmail.com>
2025-02-28 07:16:49 -0800

    add home repo and impl and entity

--- lib/src/features/home/data/repositories/home_repository_impl.dart
+++ lib/src/features/home/data/repositories/home_repository_impl.dart
@@ -1,6 +1,8 @@
 import 'package:aban_tether/src/data/cryptocurrency_service/ctypto/data_source/crypto_remote_datasource.dart';
-import 'package:aban_tether/src/data/cryptocurrency_service/favorite/data_source/favorite_remote_datasource.dart';
-import 'package:aban_tether/src/features/home/domain/models/entities/cryptocurrency_entity.dart';
+import 'package:aban_tether/src/data/cryptocurrency_service/favorite/data_source/favorites_remote_datasource.dart';
+import 'package:aban_tether/src/features/home/domain/models/entities/coin_entity.dart';
+import 'package:aban_tether/src/features/home/domain/models/entities/favorite_coin_entity.dart';
+import 'package:aban_tether/src/features/home/domain/models/param_models/change_crypto_favorite_state_param.dart';
 import 'package:aban_tether/src/features/home/domain/repositories/home_repository.dart';
 
 class HomeRepoImpl extends HomeRepo{
@@ -10,20 +12,28 @@ class HomeRepoImpl extends HomeRepo{
   HomeRepoImpl({required this.cryptoRemoteDatasource, required this.favoriteRemoteDatasource});
 
   @override
-  Future<CryptoCurrencyEntity> getCryptoCurrencies() {
-    // TODO: implement getCryptoCurrencies
+  Future<void> addCoinToFavorite(ChangeFavoriteCoinStateParam param) {
+    // TODO: implement addCoinToFavorite
     throw UnimplementedError();
   }
 
   @override
-  Future<CryptoCurrencyEntity> addToFavorite() {
-    // TODO: implement addToFavorite
+  Future<void> deleteCoinFromFavorite(ChangeFavoriteCoinStateParam param) {
+    // TODO: implement deleteCoinFromFavorite
     throw UnimplementedError();
   }
 
   @override
-  Future<CryptoCurrencyEntity> remoteFromFavorite() {
-    // TODO: implement remoteFromFavorite
+  Future<List<CoinEntity>> getAllCoins() {
+    // TODO: implement getAllCoins
     throw UnimplementedError();
   }
+
+  @override
+  Future<List<FavoriteCoinEntity>> getFavoriteCoins() {
+    // TODO: implement getFavoriteCoins
+    throw UnimplementedError();
+  }
+
+
 }

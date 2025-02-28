786dada9f06d22c2da87cd9a7c339abea6bf5563
sepehr13494 <marash.sepehr13494@gmail.com>
2025-02-28 07:57:46 -0800

    add favorite functions to home repo

--- lib/src/features/home/data/repositories/home_repository_impl.dart
+++ lib/src/features/home/data/repositories/home_repository_impl.dart
@@ -1,8 +1,14 @@
 import 'package:aban_tether/src/data/cryptocurrency_service/ctypto/data_source/crypto_remote_datasource.dart';
 import 'package:aban_tether/src/data/cryptocurrency_service/favorite/data_source/favorites_remote_datasource.dart';
+import 'package:aban_tether/src/data/cryptocurrency_service/favorite/models/response_models/get_favorite_coin_response.dart';
+import 'package:aban_tether/src/features/home/data/mapper/add_coin_to_favorite_param_ext.dart';
+import 'package:aban_tether/src/features/home/data/mapper/cryptocurrency_response_ext.dart';
+import 'package:aban_tether/src/features/home/data/mapper/delete_coin_from_favorite_param_ext.dart';
+import 'package:aban_tether/src/features/home/data/mapper/get_favorite_coins_response_ext.dart';
 import 'package:aban_tether/src/features/home/domain/models/entities/coin_entity.dart';
 import 'package:aban_tether/src/features/home/domain/models/entities/favorite_coin_entity.dart';
-import 'package:aban_tether/src/features/home/domain/models/param_models/change_crypto_favorite_state_param.dart';
+import 'package:aban_tether/src/features/home/domain/models/param_models/add_coin_to_favorite_param.dart';
+import 'package:aban_tether/src/features/home/domain/models/param_models/delete_coin_from_favorite_param.dart';
 import 'package:aban_tether/src/features/home/domain/repositories/home_repository.dart';
 
 class HomeRepoImpl extends HomeRepo{
@@ -11,28 +17,26 @@ class HomeRepoImpl extends HomeRepo{
 
   HomeRepoImpl({required this.cryptoRemoteDatasource, required this.favoriteRemoteDatasource});
 
-  @override
-  Future<void> addCoinToFavorite(ChangeFavoriteCoinStateParam param) {
-    // TODO: implement addCoinToFavorite
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<void> deleteCoinFromFavorite(ChangeFavoriteCoinStateParam param) {
-    // TODO: implement deleteCoinFromFavorite
-    throw UnimplementedError();
-  }
 
   @override
   Future<List<CoinEntity>> getAllCoins() {
-    // TODO: implement getAllCoins
-    throw UnimplementedError();
+    return cryptoRemoteDatasource.getCryptoCurrencies().then((response) => response.map((e) => e.toEntity()).toList(),);
   }
 
   @override
   Future<List<FavoriteCoinEntity>> getFavoriteCoins() {
-    // TODO: implement getFavoriteCoins
-    throw UnimplementedError();
+    return favoriteRemoteDatasource.getFavoriteCoins().then((response) => (response).map((e) => e.toEntity(),).toList(),);
+  }
+
+  @override
+  Future<int> addCoinToFavorite(AddCoinToFavoriteParam param) {
+    return favoriteRemoteDatasource.addCoinToFavorite(param.toRequest()).then((response) => response.id!,);
+  }
+
+  @override
+  Future<void> deleteCoinFromFavorite(DeleteCoinFromFavoriteParam param) {
+    return favoriteRemoteDatasource.deleteCoinFromFavorite(param.toRequest());
+
   }
 
 
--- lib/src/features/home/domain/repositories/home_repository.dart
+++ lib/src/features/home/domain/repositories/home_repository.dart
@@ -1,10 +1,11 @@
 import 'package:aban_tether/src/features/home/domain/models/entities/coin_entity.dart';
 import 'package:aban_tether/src/features/home/domain/models/entities/favorite_coin_entity.dart';
-import 'package:aban_tether/src/features/home/domain/models/param_models/change_crypto_favorite_state_param.dart';
+import 'package:aban_tether/src/features/home/domain/models/param_models/add_coin_to_favorite_param.dart';
+import 'package:aban_tether/src/features/home/domain/models/param_models/delete_coin_from_favorite_param.dart';
 
 abstract class HomeRepo{
   Future<List<CoinEntity>> getAllCoins();
   Future<List<FavoriteCoinEntity>> getFavoriteCoins();
-  Future<void> addCoinToFavorite(ChangeFavoriteCoinStateParam param);
-  Future<void> deleteCoinFromFavorite(ChangeFavoriteCoinStateParam param);
+  Future<int> addCoinToFavorite(AddCoinToFavoriteParam param);
+  Future<void> deleteCoinFromFavorite(DeleteCoinFromFavoriteParam param);
 }
